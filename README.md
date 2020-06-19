9a93a1ae427f02d69989d62c5b5347648148597c
Ian Magenta <ianmagenta@gmail.com>
2020-06-18 20:38:47 -0700

    updated readme

--- README.md
+++ README.md
@@ -7,3 +7,14 @@
 The world of game development is flooded with all sorts of helpful tools. From engines, to libraries, to level editors, it has never been easier to make a game. But what's the right tool for the job? And with so many out there, are you missing one that could slot perfectly into your next project?
 
 That's where **Game Dev Toolbox** (**GDT**) comes in! **GDT** makes it simple to search through and discover a variety of tools that make game development fast and fun. Assemble your favorites with the site's Build Editor and (if you'd like) share with the world all of the technologies your game is built on.
+
+## Technologies Used
+
+- React
+- Auth0
+- Semantic UI
+- Flask
+
+## Hey! I wanna see the backend of this thing.
+
+Really? Ok, here you go: https://github.com/ianmagenta/Game-Dev-Toolbox-Backend
--- src/react-auth0-spa.js
+++ src/react-auth0-spa.js
@@ -31,26 +31,20 @@ export const Auth0Provider = ({ children, onRedirectCallback = DEFAULT_REDIRECT_
 
       if (isAuthenticated) {
         const user = await auth0FromHook.getUser();
-        const token = await auth0FromHook.getTokenSilently(); //custom
-        setUser(user);
 
         // Everything in this block was added by me.
-        try {
-          console.log("user time: ", user.id);
-          console.log("Token time: ", token);
-          const res = await axios({
-            url: `${api}/users`,
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${token}`,
-            },
-            method: "post",
-            data: JSON.stringify("hello"),
-          });
-        } catch (err) {
-          console.error("It's error time: ", err);
-        }
+        const token = await auth0FromHook.getTokenSilently(); //custom
+        await axios({
+          url: `${api}/users`,
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          method: "post",
+          data: user.id,
+        });
         // End of custom block.
+
+        setUser(user);
       }
 
       setLoading(false);

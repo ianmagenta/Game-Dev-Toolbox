f92e8c8f7931e2cfa76194be8c1cf364476072d4
Ian Magenta <ianmagenta@gmail.com>
2020-06-26 12:40:29 -0700

    updated api url

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const api =
-  process.env.NODE_ENV === "development" ? "http://localhost:5000" : "https://game-dev-toolbox-api.herokuapp.com/";
+  process.env.NODE_ENV === "development" ? "http://localhost:5000" : "https://game-dev-toolbox-api.herokuapp.com";
